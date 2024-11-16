e0db895f968db13f1a3d1e75e167cb8f24273142
CharCarvalho <charlescarvalho97@outlook.com>
2024-11-16 11:42:33 -0300

    Alteração na estrutura do .env

--- src/main/java/com/globalsolution2/fiap/GlobalSolution2Application.java
+++ src/main/java/com/globalsolution2/fiap/GlobalSolution2Application.java
@@ -10,10 +10,13 @@ public class GlobalSolution2Application {
 
 	public static void main(String[] args) {
 		
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		if (System.getenv("SPRING_DATASOURCE_URL") == null) {
+		    Dotenv dotenv = Dotenv.load();
+		    System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		    System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		    System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		}
+
         
 		SpringApplication.run(GlobalSolution2Application.class, args);
 	}
